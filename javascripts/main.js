ec90f1741d2dd6643c36de0a545a534ea2b153c0
broche sophie <brochesophie@mbp-de-broche.paris.zenexity.com>
2014-02-28 18:36:23 +0100

    modif css + javascript header

---
+++ javascripts/main.js
@@ -0,0 +1,44 @@
+$(function (){
+	//code
+	//alert('salut');
+	//$('.icon-localisation').css('color','red');
+	var oldPositionMouseX;
+
+	//Taille de l'écran
+	var wi = $(window).width();
+	var newPositionMouseX;
+	var size_bigPosition;
+	var size_middlePosition;
+	var size_littlePosition;
+
+	$( document ).on( "mousemove", function( event ) {
+
+		newPositionMouseX = event.pageX;
+		size_bigPosition = $("#size_big").position();
+		size_middlePosition = $("#size_medium").position();
+		size_littlePosition = $("#size_little").position();
+
+		if(newPositionMouseX < oldPositionMouseX){
+			
+			$('#size_big').css('left', (newPositionMouseX - wi/2)/20 +'px' );
+			$('#size_medium').css('left', (newPositionMouseX - wi/2)/15 +'px' );
+			$('#size_little').css('left', (newPositionMouseX - wi/2)/10 +'px' );
+			oldPositionMouseX = newPositionMouseX;
+			console.log(wi/2);
+		}
+
+		else if(newPositionMouseX > oldPositionMouseX){
+			$('#size_big').css('left', (newPositionMouseX - wi/2)/20 +'px' );
+			$('#size_medium').css('left', (newPositionMouseX - wi/2)/15 +'px' );
+			$('#size_little').css('left', (newPositionMouseX - wi/2)/10 +'px' );
+			oldPositionMouseX = newPositionMouseX;
+			console.log(wi/2);
+		}
+		else{
+			oldPositionMouseX = newPositionMouseX;
+		}
+		//$('#size_big').css('left', positionbigX + 10 + 'px' );
+		//console.log(positionbigX + "tata");
+	});
+
+});
